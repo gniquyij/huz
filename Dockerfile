ce6ae248ee3991dd9553eb56695119b9633c16cf
yuqing.ji <yuqing.ji@outlook.com>
2019-10-06 20:49:03 +0800

    update cronjob configure

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ MAINTAINER vjyq <yuqing.ji@outlook.com>
 WORKDIR /huz
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
-RUN apt-get update -y && \
-    apt-get install -y ffmpeg
+RUN apt-get update -y
+RUN apt-get install -y ffmpeg
+RUN apt-get install -y cron
 CMD find . \( -name '__pycache__' -or -name '*.pyc' \) -delete
 CMD tail -f /dev/null
--- init.sh
+++ init.sh
@@ -9,10 +9,10 @@ cd ${here}/prepost/
 python pre.py
 
 echo '''
-* * * * * cd '"${here}"'/recorders && /Users/yuqing.ji/.pyenv/versions/3.7.1/envs/huz/bin/python recording.py
-@weekly cd '"${here}"'/recorders && /Users/yuqing.ji/.pyenv/versions/3.7.1/envs/huz/bin/python playback.py weekly
-@monthly cd '"${here}"'/recorders && /Users/yuqing.ji/.pyenv/versions/3.7.1/envs/huz/bin/python playback.py monthly
-@yearly cd '"${here}"'/recorders && /Users/yuqing.ji/.pyenv/versions/3.7.1/envs/huz/bin/python playback.py yearly
+* * * * * cd '"${here}"'/recorders && /usr/local/bin/python recording.py
+@weekly cd '"${here}"'/recorders && /usr/local/bin/python playback.py weekly
+@monthly cd '"${here}"'/recorders && /usr/local/bin/python playback.py monthly
+@yearly cd '"${here}"'/recorders && /usr/local/bin/python playback.py yearly
 ''' > crontmp
 crontab crontmp
 rm crontmp
