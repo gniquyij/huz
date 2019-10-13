895efe42d071750ec02f3733e0c20a4638669420
yuqing.ji <yuqing.ji@outlook.com>
2019-10-13 08:24:08 +0800

    init visualization

--- settings.py
+++ settings.py
@@ -9,14 +9,14 @@ if not os.path.isdir('%s/recorders/tmp' % HUZ_HOME_PATH):
      os.mkdir('%s/recorders/tmp' % HUZ_HOME_PATH)
 HUZ_RECORDING_TMP_PATH = os.path.join(HUZ_HOME_PATH, 'recorders/tmp')
 
-
 #####################
 # psql database
 #####################
 
 HUZ_PSQL_DB_NAME = 'huz'
-HUZ_PSQL_USER = 'huzer'
-HUZ_PSQL_PASSWORD = ''
 HUZ_PSQL_HOST = '<ip-address>'
+HUZ_PSQL_PASSWORD = 'password'
 HUZ_PSQL_PORT = '5432'
+HUZ_PSQL_USER = 'huzer'
+
 HUZ_PSQL_CONNECT_CMD = 'dbname=%s user=%s host=%s password=%s' % (HUZ_PSQL_DB_NAME, HUZ_PSQL_USER, HUZ_PSQL_HOST, HUZ_PSQL_PASSWORD)
