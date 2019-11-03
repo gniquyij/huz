1009758fe9a3ad934a8bf9be4b3ad09c3f7b3daf
yuqing.ji <yuqing.ji@outlook.com>
2019-11-03 09:20:45 +0800

    make huz work with localhost

--- settings.py
+++ settings.py
@@ -2,7 +2,6 @@
 
 import os
 
-
 HUZ_HOME_PATH = os.path.dirname(os.path.realpath(__file__))
 HUZ_SRC_PATH = os.path.join(HUZ_HOME_PATH, 'src')
 if not os.path.isdir('%s/recorders/tmp' % HUZ_HOME_PATH):
@@ -10,11 +9,11 @@ if not os.path.isdir('%s/recorders/tmp' % HUZ_HOME_PATH):
 HUZ_RECORDING_TMP_PATH = os.path.join(HUZ_HOME_PATH, 'recorders/tmp')
 
 #####################
-# psql database
+# postgres
 #####################
 
 HUZ_PSQL_DB_NAME = 'huz'
-HUZ_PSQL_HOST = '<ip-address>'
+HUZ_PSQL_HOST = '172.17.0.1'
 HUZ_PSQL_PASSWORD = 'password'
 HUZ_PSQL_PORT = '5432'
 HUZ_PSQL_USER = 'huzer'
